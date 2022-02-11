e848f85aad16359a25c16e5a3ec8c5c632da70e6
jpedram <jpedram@amazon.com>
2022-02-11 15:14:17 +0000

    all commit1

--- pipeline/pipeline/pipeline_stack.py
+++ pipeline/pipeline/pipeline_stack.py
@@ -14,3 +14,57 @@ class PipelineStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        artifacts_bucket = s3.Bucket(self, "ArtifactsBucket")
+        
+   ###source stage
+     # Import existing CodeCommit sam-app repository
+        code_repo = codecommit.Repository.from_repository_name(self, "AppRepository", "sam-app")
+        
+        # Pipeline creation starts
+        pipeline = codepipeline.Pipeline(self, "Pipeline",
+          artifact_bucket = artifacts_bucket
+        )
+        
+        # Declare source code as an artifact
+        source_output = codepipeline.Artifact()
+        
+        # Add source stage to pipeline
+        pipeline.add_stage(
+            stage_name = "Source", 
+            actions = [ 
+                codepipeline_actions.CodeCommitSourceAction(
+                    action_name = "Source",
+                    branch = "main",
+                    output = source_output,
+                    repository = code_repo,
+                )
+            ]
+        )
+
+
+###add build stage
+        # Declare build output as artifacts
+        build_output = codepipeline.Artifact()
+        
+        # Declare a new CodeBuild project
+        build_project = codebuild.PipelineProject(self, "Build", 
+            environment = codebuild.BuildEnvironment(
+                build_image = codebuild.LinuxBuildImage.STANDARD_5_0,
+            ),
+            environment_variables = {
+                'PACKAGE_BUCKET': codebuild.BuildEnvironmentVariable(value = artifacts_bucket.bucket_name),
+            },
+        )
+        
+        # Add the build stage to our pipeline
+        pipeline.add_stage(
+            stage_name = "Build", 
+            actions = [
+                codepipeline_actions.CodeBuildAction(
+                    action_name = "Build",
+                    project = build_project,
+                    input = source_output,
+                    outputs = [build_output],
+                )
+            ]
+        )
